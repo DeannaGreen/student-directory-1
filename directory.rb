131c9b6b75d12b64ca19021c7148275b31f9c407
Deanna Green <deanna@16vmini.co.uk>
2019-03-15 10:47:48 +0000

    added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,25 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students and their cohort"
   puts "To finish, just hit return twice"
@@ -35,6 +57,9 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(120)
 end
 
+interactive_menu
+
+
 def print_letter(students)
   puts "Enter a letter:"
   letter = STDIN.gets.chomp
@@ -71,8 +96,3 @@ def print_by_cohort(students)
     end
   end
 end
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
