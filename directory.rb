569e40af591458bccd4cd88e808df1f4259ccd0c
Deanna Green <deanna@16vmini.co.uk>
2019-03-15 11:03:10 +0000

    added option to save data

--- directory.rb
+++ directory.rb
@@ -1,35 +1,4 @@
-def interactive_menu
-  @students = []
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
-def process(selection)
-  case selection
-  when "1"
-    students = input_students
-  when "2"
-    show_students
-  when "9"
-    exit
-  else
-    puts "I don't know what you meant, try again"
-  end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_student_list(@students)
-  print_footer(@students)
-end
+@students = []
 
 def input_students
   puts "Please enter the names of the students and their cohort"
@@ -50,6 +19,41 @@ def input_students
   @students
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def print_header
   puts "The students of Villians Academy".center(120)
   puts "-------------".center(120)
@@ -65,6 +69,16 @@ end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(120)
+end
+
+def save_students
+  file = File.open("studnets.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
